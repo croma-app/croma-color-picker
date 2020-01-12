7c005be5fc017f18869a51553664e84026af915c
kamal <kkjoshi.it@gmail.com>
2020-01-12 10:33:33 +0530

    bug fixes and styling

--- CromaColorPicker.js
+++ CromaColorPicker.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, TextInput} from 'react-native';
 import Color from 'pigment/full';
 import Touchable from 'react-native-platform-touchable';
 const colors = [
@@ -7,17 +7,17 @@ const colors = [
   '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#f44336', '#e00032'
 ];
 function CromaColorPicker(props) {
-  const {primarySelectedColor, setPrimarySelectedColor} = useState("#db0a5b");
-  const {selectedColor, setSelectedColor} = useState("#db0A5b");
+  const [primarySelectedColor, setPrimarySelectedColor] = useState("#db0a5b");
+  const [selectedColor, setSelectedColor] = useState("#db0A5b");
   function renderShades(h) {
     console.log("h=" + h);
-    let w = 100, b, color;
+    let w = 100, b;
     let colors = [];
     for (let i = 0; i < 8; i++) {
         b = 10;
-  
+      
         for (let j = 0; j < 10; j++) {
-            color = new Color(`hsl(${h},${w},${b})`).tohex();
+            let color = new Color(`hsl(${h},${w},${b})`).tohex();
             colors.push(color);
             b += 9;
         }
@@ -30,23 +30,24 @@ function CromaColorPicker(props) {
     <View props={props} style={[...props.style, styles.container]}>
       
       <View style={styles.primaryColorsView}>
-        {colors.map(color => <ColorBox onPress={() => setPrimarySelectedColor(color)} color={color} style={[styles.primaryColorBox]}/>)}
+        {colors.map(color => <ColorBox onPress={() => {setPrimarySelectedColor(color); setSelectedColor(color);}} color={color} style={[styles.primaryColorBox]}/>)}
       </View>
       <View style={styles.shadesView}>
         {renderShades(new Color(primarySelectedColor).hsl[0])}
       </View>
-      <View style={styles.selectedColorView}></View>
-      <TextInput 
-          style={styles.input}
-          value={selectedColor}
-          onChangeText={color => {}}
+      <View style={styles.selectedColorView}>
+        <TextInput 
+            style={styles.input}
+            value={selectedColor}
+            onChangeText={color => {}}
         />
-        <View style={[style.selectedColor, {backgroundColor: selectedColor}]}></View>
+        <View style={[styles.selectedColor, {backgroundColor: selectedColor}]}></View>
+      </View>
     </View>
   )
 }
 function ColorBox(props) {
-  return <Touchable props={props} style={[props.style, styles.colorBox, {backgroundColor: props.color}]} background={Touchable.Ripple("white", true)}>
+  return <Touchable onPress={props.onPress} props={props} style={[props.style, styles.colorBox, {backgroundColor: props.color}]} background={Touchable.Ripple("white", true)}>
     <View/>
   </Touchable>
 }
@@ -54,12 +55,23 @@ function ColorBox(props) {
 const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
+    flex: 1,
+    alignItems: "flex-start",
   },
   primaryColorsView: {
-    width: "100%",
-    height: "20%",
+    flex: 1,
     flexDirection: "row",
     flexWrap: "wrap",
+    alignItems: "flex-start",
+  },
+  shadesView: {
+    flex: 4,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  selectedColorView: {
+    flexDirection: "row",
+    flex: 1,
   },
   colorBox: {
     width: "10%",
@@ -68,25 +80,17 @@ const styles = StyleSheet.create({
     height: "50%",
   },
   shadesColorBox: {
-    height: "12.5%",
+    height: "12%",
   }, 
-  shadesView: {
-    height: "70%",
-    width: "100%",
-    flexDirection: "row",
-    flexWrap: "wrap",
-  },
-  selectedColorView: {
-    flex: 1,
-    flexDirection: "column",
-    height: "10%",
-    width: "100%",
-  },
+ 
   selectedColor: {
-
+    height: "100%",
+    width: "50%",
   },
   input: {
-
+    width: "50%",
+    borderBottomColor: "black",
+    borderBottomWidth: 1
   },
 });
 export {CromaColorPicker }
