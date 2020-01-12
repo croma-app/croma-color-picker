ec02b5e6e32bf0d88bdd34852ac397dd7bdf1f89
kamal <kkjoshi.it@gmail.com>
2020-01-12 17:25:47 +0530

    make styling work on react native web also

--- CromaColorPicker.js
+++ CromaColorPicker.js
@@ -6,6 +6,8 @@ const colors = [
   '#f50057', '#db0A5b', '#c51162', '#9c27b0', '#673ab7', '#4b77be', '#2196f3', '#03a9f4', '#00bcd4', '#1bbc9b',
   '#009688', '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722', '#f44336', '#e00032'
 ];
+const colorsRow1 = colors.slice(0, 10);
+const colorsRow2 = colors.slice(10, 20);
 function CromaColorPicker(props) {
   const [primarySelectedColor, setPrimarySelectedColor] = useState("#db0a5b");
   const [selectedColor, _setSelectedColor] = useState("#db0A5b");
@@ -20,28 +22,36 @@ function CromaColorPicker(props) {
     console.log("h=" + h);
     let w = 100, b;
     let colors = [];
+    
     for (let i = 0; i < 8; i++) {
         b = 10;
-      
+        let rows = [];
         for (let j = 0; j < 10; j++) {
             let color = new Color(`hsl(${h},${w},${b})`).tohex();
-            colors.push(color);
+            rows.push(color);
             b += 9;
         }
+        colors.push(rows);
   
         w -= 12;
     }
-    return colors.map((color) => <ColorBox style={[styles.shadesColorBox]} onPress={() => setSelectedColor(color)} color={color}/>);
+    
+    return colors.map((row) => <View style={styles.shadesView}> 
+      {row.map((color) => <ColorBox style={[styles.shadesColorBox]} onPress={() => setSelectedColor(color)} color={color}/>)}
+    </View>);
   }
   return (
     <View props={props} style={[...props.style, styles.container]}>
       
       <View style={styles.primaryColorsView}>
-        {colors.map(color => <ColorBox onPress={() => {setPrimarySelectedColor(color); setSelectedColor(color);}} color={color} style={[styles.primaryColorBox]}/>)}
+        {colorsRow1.map(color => <ColorBox onPress={() => {setPrimarySelectedColor(color); setSelectedColor(color);}} color={color} style={[styles.primaryColorBox]}/>)}
       </View>
-      <View style={styles.shadesView}>
-        {renderShades(new Color(primarySelectedColor).hsl[0])}
+      <View style={styles.primaryColorsView}>
+        {colorsRow2.map(color => <ColorBox onPress={() => {setPrimarySelectedColor(color); setSelectedColor(color);}} color={color} style={[styles.primaryColorBox]}/>)}
       </View>
+      
+      {renderShades(new Color(primarySelectedColor).hsl[0])}
+      
       <View style={styles.selectedColorView}>
         <TextInput 
             style={styles.input}
@@ -63,35 +73,29 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "column",
     flex: 1,
-    alignItems: "flex-start",
   },
   primaryColorsView: {
     flex: 1,
     flexDirection: "row",
-    flexWrap: "wrap",
-    alignItems: "flex-start",
   },
   shadesView: {
-    flex: 4,
+    flex: 1,
     flexDirection: "row",
-    flexWrap: "wrap",
   },
   selectedColorView: {
+    marginTop: 8,
     flexDirection: "row",
     flex: 1,
   },
   colorBox: {
-    width: "10%",
+    flex: 1,
   },
   primaryColorBox: {
-    height: "50%",
   },
   shadesColorBox: {
-    height: "12%",
+    
   }, 
- 
   selectedColor: {
-    height: "100%",
     width: "50%",
   },
   input: {
